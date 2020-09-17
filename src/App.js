0dc668e0d2c8fb4d94244d764a748bc7137f6601
Mohibullah Arsala <mohib-arsala@Mohib-MacBook-Pro.local>
2020-09-17 17:08:11 -0400

    new project file are added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    //BEM naming convention
+    <div className="app">
+     <h1>Lets build the facebook clone!</h1>
+
+     <Header />
+     
+      {/* SideBar */}
+      {/* Feed */}
+      {/* Widghs */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDZN9NPNwvjWs93yXAr3R1E9cp8t3KCxh8",
+    authDomain: "facebook-clone-a5811.firebaseapp.com",
+    databaseURL: "https://facebook-clone-a5811.firebaseio.com",
+    projectId: "facebook-clone-a5811",
+    storageBucket: "facebook-clone-a5811.appspot.com",
+    messagingSenderId: "200520711746",
+    appId: "1:200520711746:web:ada898fe4e519bd78bf7db",
+    measurementId: "G-QJE819GBNL"
+};
