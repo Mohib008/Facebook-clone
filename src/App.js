c53664bf6d0e43daabef52f62c56ca7936ee2a33
Mohibullah Arsala <mohib-arsala@Mohib-MacBook-Pro.local>
2020-09-29 18:28:57 -0400

    sidbarRow file added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Sidebar from './Sidebar';
 import './App.css';
 
 function App() {
@@ -7,9 +8,12 @@ function App() {
     //BEM naming convention
     <div className="app">
      <Header />
+     <div className="app__body">
+      <Sidebar />
       {/* SideBar */}
       {/* Feed */}
       {/* Widghs */}
+    </div>
     </div>
   );
 }
