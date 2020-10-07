4bb9ecb7db39405d4feb4f5ea328d2ab0ae04c0c
Mohibullah Arsala <mohib-arsala@Mohib-MacBook-Pro.local>
2020-10-07 07:59:52 -0400

    module error fixed

---
+++ build/precache-manifest.508e28086c4fe58599928152b446a2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e896fc0bf2766ffe7efa47304c98480f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72fddfcfb2cda2d11dfc",
+    "url": "/static/css/main.47ac300b.chunk.css"
+  },
+  {
+    "revision": "0e643d3dce3d8e4501af",
+    "url": "/static/js/2.17c26cff.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.17c26cff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72fddfcfb2cda2d11dfc",
+    "url": "/static/js/main.df5c982c.chunk.js"
+  },
+  {
+    "revision": "3fb696b04521c4486772",
+    "url": "/static/js/runtime-main.2e1440b3.js"
+  }
+]);
